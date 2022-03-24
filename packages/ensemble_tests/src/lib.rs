7c6d913d14ee597093e811921147e2388e26ef2e
sbeem <c1ihvzhz@4wrd.cc>
2022-03-24 00:41:27 +0000

    Allow building without ensemble with wasm32 target

--- packages/ensemble_tests/src/lib.rs
+++ packages/ensemble_tests/src/lib.rs
@@ -1,5 +1,9 @@
+#[cfg(not(target_arch = "wasm32"))]
 pub mod constants;
+
+#[cfg(not(target_arch = "wasm32"))]
 pub mod contract_helpers;
 
 #[cfg(test)]
+#[cfg(not(target_arch = "wasm32"))]
 mod test;
