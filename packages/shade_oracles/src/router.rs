77ce71100f8f3ca22a128be737ca397861652371
hydropump3 <3ki2fiay@anonaddy.me>
2022-06-02 15:29:37 -0400

    code cleanup and fixes band

--- packages/shade_oracles/src/router.rs
+++ packages/shade_oracles/src/router.rs
@@ -87,32 +87,6 @@ pub mod querier {
     use super::QueryMsg;
     use super::*;
 
-    /// Gets the price returned by the oracle stored at key
-    pub fn query_price(
-        contract: &Contract,
-        querier: &impl Querier,
-        key: String,
-    ) -> StdResult<OraclePrice> {
-        querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
-            contract_addr: contract.address.clone(),
-            callback_code_hash: contract.code_hash.clone(),
-            msg: to_binary(&QueryMsg::GetPrice { key })?,
-        }))
-    }
-
-    /// Gets the prices returned by the oracles stored at that key
-    pub fn query_prices(
-        contract: &Contract,
-        querier: &impl Querier,
-        keys: Vec<String>,
-    ) -> StdResult<Vec<OraclePrice>> {
-        querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
-            contract_addr: contract.address.clone(),
-            callback_code_hash: contract.code_hash.clone(),
-            msg: to_binary(&QueryMsg::GetPrices { keys })?,
-        }))
-    }
-
     // Gets the oracle contract stored at key
     pub fn query_oracle(
         contract: &Contract,
