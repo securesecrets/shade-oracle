6afb14b8fddbaf276ff677dd13e2654b905084cc
hydropump3 <3ki2fiay@anonaddy.me>
2022-08-02 00:42:28 -0400

    updates oracles to start using new math lib

--- packages/shade_oracles/src/lib.rs
+++ packages/shade_oracles/src/lib.rs
@@ -16,6 +16,8 @@ pub mod core {
     pub use schemars;
     pub use serde;
     pub use thiserror;
+    pub use ethnum;
+    pub use better_secret_math;
     pub use shade_admin::admin::validate_admin;
     pub use shade_protocol::utils::{Query, InstantiateCallback, ExecuteCallback, pad_handle_result, pad_query_result};
     pub use shade_protocol::utils::generic_response::ResponseStatus;
